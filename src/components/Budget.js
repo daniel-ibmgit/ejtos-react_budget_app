2e4a09f640abdc0c95a46ea81e78f6d9f97336a4
daniel-ibmgit <byiringirodaniel2001@gmail.com>
2022-12-31 18:11:00 -0500

    new updates

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,13 +2,35 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+	const { budget,dispatch,expenses, currency } = useContext(AppContext);
+    const max_budget = 20000;
 
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+	const changeBudget = (val) =>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+
+		if(val<totalExpenses) {
+			alert("You cannot reduce the budget that is already allocated!");
+		}
+
+        else if (val > max_budget) {
+            alert("Budget cannot exceed £" + max_budget);
+        }
+        else {
+			dispatch({
+				type: 'SET_BUDGET',
+				payload: val,
+			})
+			}
+	}
+	
+	return (
+		<div className='alert alert-secondary'>
+            <span>Budget: {currency}</span>
+			<input class="w-50" type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
+		</div>
+	);
 };
 
 export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,39 +1,58 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { TiDelete} from 'react-icons/ti';
+import {IoIosAddCircle, IoIosRemoveCircle } from "react-icons/io"
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+	const { dispatch, currency } = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
-    };
+	const handleDeleteExpense = () => {
+		dispatch({
+			type: 'DELETE_EXPENSE',
+			payload: props.id,
+		});
+	};
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
+	const increaseAllocation = (name) => {
+		const expense = {
+			name: name,
+			cost: 10,
+		};
+    
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
+		dispatch({
+			type: 'ADD_EXPENSE',
+			payload: expense
+		});
 
-    }
+	}
+
+    const decreaseAllocation = (name) => {
+		const expense = {
+			name: name,
+			cost: 10,
+		};
+    
+
+		dispatch({
+			type: 'RED_EXPENSE',
+			payload: expense
+		});
+
+	}
+
+    
 
 
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+	return (
+		<tr>
+		<td>{props.name}</td>
+		<td>{currency}{props.cost}</td>
+		<td><IoIosAddCircle fill="green" size="2em" onClick={event=> increaseAllocation(props.name)}></IoIosAddCircle></td>
+        <td><IoIosRemoveCircle fill="red" size="2em"onClick={event=> decreaseAllocation(props.name)}></IoIosRemoveCircle></td>
+		<td><TiDelete size='2em' onClick={handleDeleteExpense}></TiDelete></td>
+		</tr>
+	);
 };
 
 export default ExpenseItem;
